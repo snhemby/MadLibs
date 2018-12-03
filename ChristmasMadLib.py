1c33d3a8eea7c3fefe8f4ba911f4172ad71a5b79
snhemby <43935705+snhemby@users.noreply.github.com>
2018-12-02 20:24:18 -0600

    Add files via upload

---
+++ ChristmasMadLib.py
@@ -0,0 +1,24 @@
+print ('Hello there! Thank you for participating in this Christmas Mad Lib. Just type information as you are prompted. It is that easy!')
+
+adjective_1 = input ('Please name an adjective:')
+adjective_2 = input ('Please name an adjective:')
+adjective_3 = input ('Please name an adjective:')
+room = input ('Please name a room name:')
+plural_noun_1 = input ('Please name a plural noun:')
+plural_noun_2 = input ('Please name a plural noun:')
+plural_noun_3 = input ('Please name a plural noun:')
+noun_1 = input ('Please name a noun:')
+noun_2 = input ('Please name a noun:')
+noun_3 = input ('Please name a noun:')
+noun_4 = input ('Please name a noun:')
+noun_5 = input ('Please name a noun:')
+number = input ('Please name a number:')
+color_1 = input ('Please name a color:')
+color_2 = input ('Please name another color:')
+something_alive = input ('Please name something alive:')
+
+mad_lib = ('No Christmas season can be really ' + adjective_1 + ' unless you have a/an ' + adjective_2 + ' tree in your ' + room + '. If you live in the city, you will see many vacant ' + plural_noun_1 + ' filled with hundreds of ' + plural_noun_2 + ' for sale. If you live in the country, you can get your own ' + noun_1 + ' right out of the forest. Go out with a/an ' + noun_2 + ' and ' + noun_3 + ', and when you see a/an ' + adjective_3 + ' tree you like, you can dig it up and plant it in a/an ' + noun_4 + '. Then you can use it for ' + number + ' years. To make sure your tree is fresh, shake the branches and see if the ' + plural_noun_3 + ' fall off onto the ' + noun_5 + '. And make sure the tree is very ' + color_1 + '. Nothing looks worse than a/an ' + color_2 + ' tree. Just follow these directions and you can have a perfectly beautiful ' + something_alive + ' in your room for the season. Happy Holidays and Merry Christmas!')
+
+print (mad_lib)
+
+
